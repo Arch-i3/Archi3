e8da00164b1fb83c8b49fe7f01335ef32cc6d3ca
Sándor Kakuk <53403093+sandorkakuk@users.noreply.github.com>
2025-07-18 16:03:52 +0200

    Delete archiso/airootfs/etc/skel/run_gnome_software_once.sh
    Signed-off-by: Sándor Kakuk <53403093+sandorkakuk@users.noreply.github.com>

--- archiso/airootfs/etc/skel/run_gnome_software_once.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-FLAG_FILE="$HOME/.config/.gnome_software_launched"
-
-if [ ! -f "$FLAG_FILE" ]; then
-    gnome-software &
-    mkdir -p "$(dirname "$FLAG_FILE")"
-    touch "$FLAG_FILE"
-fi
